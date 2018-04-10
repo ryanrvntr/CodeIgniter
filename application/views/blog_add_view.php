02b6aad9402393e2250f75aa4f2b02230a7b9519
DESKTOP-ANU956C\ryanrvntr <ryanrvntr8@gmail.com>
2018-04-10 21:30:06 +0700

    UPLOAD GAMBAR

--- application/views/blog_add_view.php
+++ application/views/blog_add_view.php
@@ -25,38 +25,39 @@
         </ul>
       </div>
     </nav>
-    <main role="main" class="container">
-      <div class="row">
-        <div class="col">
-        <form action="<?php echo base_url('index.php/Blog/add_action') ?>" method="post">
-        <div class="form-group">
-        <label for="id">Id</label>
-        <input type="text" class="form-control" id="id" name="id" placeholder="Id">
-        </div>
-        <div class="form-group">
-        <label for="author">Author</label>
-        <input type="text" class="form-control" id="author" name="author" placeholder="Author">
-        </div>
-        <div class="form-group">
-        <label for="date">Date</label>
-        <input type="date" class="form-control" id="date" name="date" placeholder="Date">
-        </div>
-        <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Title">
-        </div>
-        <div class="form-group">
-        <label for="content">Content</label>
-        <input type="text" class="form-control" id="content" name="content" placeholder="Content">
-        </div>
-        <div class="form-group">
-        <label for="image_file">Image</label>
-        <input type="text" class="form-control" id="image_file" name="image_file" placeholder="Image">
-        </div>
-        <input type="submit" name="add" value="Tambah" class="btn btn-success">
-        </form>
-        </div>
-      </div>
+            <main role="main" class="container"> 
+      <div class="row"> 
+        <div class="col"> 
+          <?php echo  $error ?> 
+          <?php echo form_open_multipart('Blog/add_action');?> 
+            <div class="form-group"> 
+    <label for="id">Id</label> 
+    <input type="text" class="form-control" id="id" name="id" placeholder="Id"> 
+  </div> 
+  <div class="form-group"> 
+    <label for="author">Author</label> 
+    <input type="text" class="form-control" id="author" name="author" placeholder="Author"> 
+  </div> 
+<div class="form-group"> 
+    <label for="date">Date</label> 
+    <input type="date" class="form-control" id="date" name="date" placeholder="Date"> 
+  </div> 
+  <div class="form-group"> 
+    <label for="title">Title</label> 
+    <input type="text" class="form-control" id="title" name="title" placeholder="Title"> 
+  </div> 
+  <div class="form-group"> 
+    <label for="content">Content</label> 
+    <input type="text" class="form-control" id="content" name="content" placeholder="Content"> 
+  </div> 
+  <div class="form-group"> 
+    <label for="image_file">Image</label> 
+    <input type = "file" name = "image_file" size = "20" />  
+  </div> 
+  <input type="submit" name="add" value="Tambah" class="btn btn-success"> 
+          </form> 
+        </div> 
+      </div> 
     </main>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
--- application/views/blog_list.php
+++ application/views/blog_list.php
@@ -28,24 +28,22 @@
         </ul>
       </div>
     </nav>
-    <main role="main" class="container">
-      <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a>
-      <br>
-      <br>
-      <ul class="list-unstyled">
-      <?php foreach ($records as $key => $value): ?>
-        <li class="media">
-          <img class="mr-3" src="http://2.bp.blogspot.com/-NBHPU2GYupE/VU7cOUlO-XI/AAAAAAAAJRg/h76CEfX96BE/s1600/gambar%2Bhati%2B(5).jpg" alt="Generic placeholder image" width="100px" height="150px">
-          <div class="media-body">
-            <h6 class="text-muted"><?php echo $value['date'] ?></h6>
-            <h5 class="mt-0 mb-1"><?php echo $value['title'] ?></h5>
-            <?php echo $value['content'] ?>
-            <br>
-              <a href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">View Details</a>
-          </div>
-      </li>
-      <?php endforeach ?>
-      </ul>
+<main role="main" class="container"> 
+      <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a> 
+      <ul class="list-unstyled"> 
+  <?php foreach ($records as $key => $value): ?> 
+    <li class="media"> 
+    <img class="mr-3" src="<?php echo base_url() ?>uploads/<?php echo $value['image_file'] ?>" alt="Generic placeholder image" width="100px" height="150px"> 
+    <div class="media-body"> 
+      <h5 class="mt-0 mb-1"><?php echo $value['title'] ?></h5> 
+      <h6 class="text-muted"><?php echo $value['date'] ?></h6> 
+      <?php echo $value['content'] ?> 
+      <br> 
+      <a href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">View Details</a> 
+    </div> 
+  </li> 
+  <?php endforeach ?> 
+</ul> 
     </main>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
