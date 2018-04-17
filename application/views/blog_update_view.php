a6c422d2b350657d80580e52bb52c443f7672e55
DESKTOP-ANU956C\ryanrvntr <ryanrvntr8@gmail.com>
2018-04-17 21:07:11 +0700

    menambahkan delete dan update

--- application/views/blog_update_view.php
+++ application/views/blog_update_view.php
@@ -22,31 +22,46 @@
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Disabled</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url("index.php/Blog") ?>">Blog</a>
-          </li>
         </ul>
       </div>
     </nav>
-<main role="main" class="container"> 
-      <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a> 
-      <ul class="list-unstyled"> 
-  <?php foreach ($records as $key => $value): ?> 
-    <li class="media"> 
-    <img class="mr-3" src="<?php echo base_url() ?>uploads/<?php echo $value['image_file'] ?>" alt="Generic placeholder image" width="100px" height="150px"> 
-    <div class="media-body"> 
-      <h5 class="mt-0 mb-1"><?php echo $value['title'] ?></h5> 
-      <h6 class="text-muted"><?php echo $value['date'] ?></h6> 
-      <?php echo $value['content'] ?> 
-      <br> 
-      <a class="btn btn-sm btn-primary" href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">View Details</a> 
-      <a class="btn btn-sm btn-success" href="<?php echo base_url('index.php/Blog/update_view/'.$value['id']) ?>">Update </a>
-      <a class="btn btn-sm btn-danger" href="<?php echo base_url('index.php/Blog/delete_action/'.$value['id']) ?>">Delete </a>
+    <main role="main" class="container"> 
+    <div class="row"> 
+      <div class="col"> 
+        <?php echo  $error ?> 
+        <?php echo  validation_errors() ?>
+        <?php echo form_open_multipart('Blog/update_view/'.$records[0]['id']);?> 
+        <div class="form-group"> 
+          <label for="id">Id</label> 
+          <input type="text" class="form-control" id="id" name="id" value="<?php echo $records[0]['id'] ?>"> 
+          <input type="hidden" name="old_id" value="<?php echo $records[0]['id']?>">
+        </div> 
+        <div class="form-group"> 
+          <label for="author">Author</label> 
+          <input type="text" class="form-control" id="author" name="author" value="<?php echo $records[0]['author'] ?>"> 
+        </div> 
+        <div class="form-group"> 
+          <label for="date">Date</label> 
+          <input type="date" class="form-control" id="date" name="date" value="<?php echo $records[0]['date'] ?>"> 
+        </div> 
+        <div class="form-group"> 
+          <label for="title">Title</label> 
+          <input type="text" class="form-control" id="title" name="title" value="<?php echo $records[0]['title'] ?>"> 
+        </div> 
+        <div class="form-group"> 
+          <label for="content">Content</label> 
+          <input type="text" class="form-control" id="content" name="content" value="<?php echo $records[0]['content'] ?>"> 
+        </div>
+        <img src="<?php echo base_url(); ?>/uploads/<?php echo $records[0]['image_file'] ?>" alt="File Tidak Ada" height="100px" width="100px">
+        <div class="form-group"> 
+          <label for="image_file">Image</label> 
+          <input type = "file" name = "image_file" size = "20" />  
+        </div> 
+        <input type="submit" name="update" value="Update" class="btn btn-success"> 
+      </form> 
     </div> 
-  </li> 
-  <?php endforeach ?> 
-</ul> 
-    </main>
+  </div> 
+</main>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
